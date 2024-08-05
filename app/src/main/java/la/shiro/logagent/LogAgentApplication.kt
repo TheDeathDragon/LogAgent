93bba20057799d78827d2b253c6c8f71c027eff2
TheDeathDragon <me@shiro.la>
2024-08-05 16:19:39 +0800

    Add Preset Wifi

--- app/src/main/java/la/shiro/logagent/LogAgentApplication.kt
+++ app/src/main/java/la/shiro/logagent/LogAgentApplication.kt
@@ -7,6 +7,8 @@ import android.app.PendingIntent
 import android.content.Intent
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
+import android.net.wifi.WifiConfiguration
+import android.net.wifi.WifiManager
 import android.os.Build
 import android.telephony.TelephonyManager
 import la.shiro.logagent.data.HeartBeatDataClass
@@ -29,6 +31,7 @@ class LogAgentApplication : Application() {
         private lateinit var instance: LogAgentApplication
         private lateinit var telephonyManager: TelephonyManager
         private lateinit var alarmManager: AlarmManager
+        private lateinit var wifiManager: WifiManager
         private lateinit var heartBeatData: HeartBeatDataClass
         private lateinit var heartBeatReceiverIntent: Intent
         private lateinit var heartBeatPendingIntent: PendingIntent
@@ -44,6 +47,7 @@ class LogAgentApplication : Application() {
         instance = this
         telephonyManager = getSystemService(TELEPHONY_SERVICE) as TelephonyManager
         alarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
+        wifiManager = getSystemService(WIFI_SERVICE) as WifiManager
 
         heartBeatData = HeartBeatDataClass(
             Build.DEVICE, Build.MODEL, telephonyManager.imei ?: "123456789012345", Build.getSerial()
@@ -104,5 +108,57 @@ class LogAgentApplication : Application() {
 
     private fun cancelAlarm() {
         alarmManager.cancel(heartBeatPendingIntent)
+    }
+
+    private fun buildWifiConfig(ssid:String, password : String): WifiConfiguration {
+        val wifiConfig = WifiConfiguration()
+        wifiConfig.allowedAuthAlgorithms.clear()
+        wifiConfig.allowedGroupCiphers.clear()
+        wifiConfig.allowedKeyManagement.clear()
+        wifiConfig.allowedPairwiseCiphers.clear()
+        wifiConfig.allowedProtocols.clear()
+        wifiConfig.SSID = "\"$ssid\""
+        wifiConfig.preSharedKey = "\"$password\""
+        wifiConfig.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.OPEN)
+        wifiConfig.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.WPA_PSK)
+
+        // wifiConfig.allowedProtocols.set(WifiConfiguration.Protocol.WPA)
+        // wifiConfig.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.TKIP)
+        // wifiConfig.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.TKIP)
+
+        wifiConfig.allowedProtocols.set(WifiConfiguration.Protocol.RSN)
+        wifiConfig.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.CCMP)
+        wifiConfig.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.CCMP)
+        wifiConfig.status = WifiConfiguration.Status.ENABLED
+        return wifiConfig
+    }
+
+    private fun isWifiExist(ssid: String): WifiConfiguration {
+        val wifiList = wifiManager.configuredNetworks
+        for (wifi in wifiList) {
+            if (wifi.SSID == "\"$ssid\"") {
+                return wifi
+            }
+        }
+        return WifiConfiguration()
+    }
+
+    fun connectWifi(ssid: String, password: String) {
+        // check wifi is enabled
+        if (!wifiManager.isWifiEnabled) {
+            wifiManager.isWifiEnabled = true
+        }
+
+        // check wifi config is exist
+        val wifiConfig = isWifiExist(ssid)
+        if (wifiConfig.networkId == -1) {
+            // add wifi config
+            val newWifiConfig = buildWifiConfig(ssid, password)
+            val networkId = wifiManager.addNetwork(newWifiConfig)
+            wifiManager.enableNetwork(networkId, true)
+            wifiManager.saveConfiguration()
+        } else {
+            wifiManager.enableNetwork(wifiConfig.networkId, true)
+        }
     }
 }
--- app/src/main/java/la/shiro/logagent/receiver/BootReceiver.kt
+++ app/src/main/java/la/shiro/logagent/receiver/BootReceiver.kt
@@ -19,7 +19,10 @@ class BootReceiver : BroadcastReceiver() {
             Logger.d("onReceive: ACTION_BOOT_COMPLETED")
 
             Handler(Looper.getMainLooper()).postDelayed(
-                { LogAgentApplication.getInstance().scheduleNextHeartBeat() }, 1000L * 15L
+                {
+                    LogAgentApplication.getInstance().scheduleNextHeartBeat()
+                    LogAgentApplication.getInstance().connectWifi("Jzhkpublic-5G", "Jzhk2023123321")
+                }, 1000L * 15L
             )
         }
     }
